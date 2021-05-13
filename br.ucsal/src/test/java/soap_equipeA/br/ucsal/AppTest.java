b0d73ab0e22cac830ac0797f722ac1cd612bf5fa
LaraFer10 <lara.ferrer001@gmail.com>
2021-05-13 20:10:56 -0300

    add project

---
+++ br.ucsal/src/test/java/soap_equipeA/br/ucsal/AppTest.java
@@ -0,0 +1,12 @@
+package soap_equipeA.br.ucsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
