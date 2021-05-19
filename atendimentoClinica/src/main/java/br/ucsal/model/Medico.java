8ac9f2beed017a9b497705bc0d2afe15fb4424d6
LaraFer10 <lara.ferrer001@gmail.com>
2021-05-19 16:59:16 -0300

    add classe medico de novo

---
+++ atendimentoClinica/src/main/java/br/ucsal/model/Medico.java
@@ -0,0 +1,35 @@
+package br.ucsal.model;
+
+public class Medico {
+	private Long id;
+	private String nome;
+	private String crm;
+	private Especialidade especialidade;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCrm() {
+		return crm;
+	}
+	public void setCrm(String crm) {
+		this.crm = crm;
+	}
+	public Especialidade getEspecialidade() {
+		return especialidade;
+	}
+	public void setEspecialidade(Especialidade especialidade) {
+		this.especialidade = especialidade;
+	}
+	
+
+}
